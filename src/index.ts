d8bd3ee1e1936208b7b7e7e1b1d06ce18d3fcc70
Fantasy <captainoftheship65@proton.me>
2026-04-03 14:24:42 +0200

    Try to optimize export order for better IDE suggestions

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,30 @@
-export {
-  validateBTC,
-  type BTCValidationResult,
-  type BitcoinAddressType,
-} from './chains/btc';
-export {
-  validateETH,
-  validateERC20,
-  type ETHValidationResult,
-  type EthereumAddressType,
-} from './chains/eth';
-export {
-  validateTRX,
-  validateTRC20,
-  type TronValidationResult,
-  type TronAddressType,
-} from './chains/trx';
-export {
-  validateBNB,
-  validateBEP20,
-  type BNBAddressType,
-  type BNBValidationResult,
-} from './chains/bnb';
-export {
-  validateXRP,
-  type XRPAddressType,
-  type XRPValidationResult,
-} from './chains/xrp';
+export {validateBTC} from './chains/btc';
+export {validateXRP} from './chains/xrp';
+export {validateETH, validateERC20} from './chains/eth';
+export {validateTRX, validateTRC20} from './chains/trx';
+export {validateBNB, validateBEP20} from './chains/bnb';
 
-export {
-  validateUSDTERC20,
-  type USDTERC20AddressType,
-  type USDTERC20ValidationResult,
-} from './aliases/usdt-erc20';
-export {
-  validateUSDTTRC20,
-  type USDTTRC20AddressType,
-  type USDTTRC20ValidationResult,
-} from './aliases/usdt-trc20';
-export {
-  validateUSDTBEP20,
-  type USDTBEP20AddressType,
-  type USDTBEP20ValidationResult,
-} from './aliases/usdt-bep20';
+export {validateUSDTERC20} from './aliases/usdt-erc20';
+export {validateUSDTTRC20} from './aliases/usdt-trc20';
+export {validateUSDTBEP20} from './aliases/usdt-bep20';
 
 export type {ValidationResult} from './types';
+
+export type {BTCValidationResult, BitcoinAddressType} from './chains/btc';
+export type {ETHValidationResult, EthereumAddressType} from './chains/eth';
+export type {TronValidationResult, TronAddressType} from './chains/trx';
+export type {BNBAddressType, BNBValidationResult} from './chains/bnb';
+export type {XRPAddressType, XRPValidationResult} from './chains/xrp';
+
+export type {
+  USDTERC20AddressType,
+  USDTERC20ValidationResult,
+} from './aliases/usdt-erc20';
+export type {
+  USDTTRC20AddressType,
+  USDTTRC20ValidationResult,
+} from './aliases/usdt-trc20';
+export type {
+  USDTBEP20AddressType,
+  USDTBEP20ValidationResult,
+} from './aliases/usdt-bep20';
