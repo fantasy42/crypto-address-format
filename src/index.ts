1e33e5e90c7acbef243a4c1d1febb43f9b8b29db
Fantasy <captainoftheship65@proton.me>
2026-05-16 15:28:31 +0200

    refactor: use wildcard type re-exports for chain/alias types

--- src/index.ts
+++ src/index.ts
@@ -9,27 +9,15 @@ export {validateUSDTERC20} from './aliases/usdt-erc20';
 export {validateUSDTTRC20} from './aliases/usdt-trc20';
 export {validateUSDTBEP20} from './aliases/usdt-bep20';
 
-export type {ValidationResult} from './types';
+export type * from './types';
 
-export type {BTCValidationResult, BitcoinAddressType} from './chains/btc';
-export type {ETHValidationResult, EthereumAddressType} from './chains/eth';
-export type {TronValidationResult, TronAddressType} from './chains/trx';
-export type {BNBAddressType, BNBValidationResult} from './chains/bnb';
-export type {XRPAddressType, XRPValidationResult} from './chains/xrp';
-export type {
-  PolygonAddressType,
-  PolygonValidationResult,
-} from './chains/polygon';
+export type * from './chains/btc';
+export type * from './chains/eth';
+export type * from './chains/trx';
+export type * from './chains/bnb';
+export type * from './chains/xrp';
+export type * from './chains/polygon';
 
-export type {
-  USDTERC20AddressType,
-  USDTERC20ValidationResult,
-} from './aliases/usdt-erc20';
-export type {
-  USDTTRC20AddressType,
-  USDTTRC20ValidationResult,
-} from './aliases/usdt-trc20';
-export type {
-  USDTBEP20AddressType,
-  USDTBEP20ValidationResult,
-} from './aliases/usdt-bep20';
+export type * from './aliases/usdt-erc20';
+export type * from './aliases/usdt-trc20';
+export type * from './aliases/usdt-bep20';
